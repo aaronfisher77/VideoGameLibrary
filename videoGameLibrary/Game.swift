a7c2ff0e4ffae5ab8eaa5a77f41315d2f78ba9ff
Aaron Fisher <aaronfisher@macbook-pro-20.local>
2018-09-10 13:17:41 -0500

    added game class

---
+++ videoGameLibrary/Game.swift
@@ -0,0 +1,24 @@
+//
+//  Game.swift
+//  videoGameLibrary
+//
+//  Created by Aaron Fisher on 9/10/18.
+//  Copyright © 2018 Aaron Fisher. All rights reserved.
+//
+
+import Foundation
+
+
+class Game {
+    let title: String
+    var checkedInOut: Bool = true
+    var dueDate: Date? = nil
+    
+    
+    init(title: String) {
+        self.title = title
+    }
+    
+}
+
+
